aec78d025f68f98ec22b083a65fc59c942e96c87
Dynamo-arg <72266387+Dynamo-arg@users.noreply.github.com>
2021-03-08 17:30:15 -0300

    Add files via upload

--- ejercicios_profundizacion.py
+++ ejercicios_profundizacion.py
@@ -39,121 +39,26 @@ def title_completed_count(userId):
     conn = TinyMongoClient()
     db = conn[db_name]
 
-    count = db.persons.find({"nationality": country}).count()
-    if userId == db.todos.find({"userId"}):
-        print("OK")
+    count = db.todos.find({"userId": userId})
+    total = 0
 
-    count = db.persons.find({"nationality": country}).count()
-
-
+    for i in count:
+        if i["completed"] == True:
+            total += 1
+      
     conn.close()
-    return userId
-
-
-def show():
-    print('Comprobemos su contenido, ¿qué hay en la tabla?')
-    # Utilizar la sentencia find para imprimir en pantalla
-    # todos los documentos de la DB
-    # Queda a su criterio serializar o no el JSON "dumps"
-    #  para imprimirlo en un formato más "agradable"
-
-    conn = TinyMongoClient()
-    db = conn[db_name]
-
-
-    cursor = db.estudiante.find()
-    data = list(cursor)
-    doc = json.dumps(data, indent=4)
-    print(doc)
-
-    conn.close()
-
-def insert_persona(name, age, grade, tutor):
-    conn = TinyMongoClient()
-    db = conn[db_name]
-
-    persona_json = {"name": name, "age": age, "grade": grade, "tutor":tutor}
-    db.estudiante.insert_one(persona_json)
-
-    conn.close()
+    return total
 
 
 
-
-def show():
-    print('Comprobemos su contenido, ¿qué hay en la tabla?')
-    # Utilizar la sentencia find para imprimir en pantalla
-    # todos los documentos de la DB
-    # Queda a su criterio serializar o no el JSON "dumps"
-    #  para imprimirlo en un formato más "agradable"
-
-    conn = TinyMongoClient()
-    db = conn[db_name]
-
-
-    cursor = db.estudiante.find()
-    data = list(cursor)
-    doc = json.dumps(data, indent=4)
-    print(doc)
-
-    conn.close()
-
-def find_by_grade(grado):
-    print('Operación búsqueda!')
-    # Utilizar la sentencia find para imprimir en pantalla
-    # aquellos estudiantes que se encuentra en en año "grade"
-
-    # De la lista de esos estudiantes debe imprimir
-    # en pantalla unicamente los siguiente campos por cada uno:
-    # id / name / age
-
-    conn = TinyMongoClient()
-    
-    db = conn[db_name]
-    person_data = db.estudiante.find({"grade": grado})
-    data = list(person_data)
-    
-    for i in data:
-        print("Nombre",i["name"], "Edad",i["age"],"Id",i["_id"])
-
-    conn.close()
-    return person_data
-
-def insert(student):
-    print('Nuevos ingresos!')
-    # Utilizar la sentencia insert_one para ingresar nuevos estudiantes
-    # a la secundaria
-
-    # El parámetro student deberá ser un JSON el cual se inserta en la db
-    conn = TinyMongoClient()
-    db = conn[db_name]
-
-    db.estudiante.insert_one(student)
-
-    conn.close()
-
-
-def count(grado):
-    print('Contar estudiantes')
-    # Utilizar la sentencia find + count para contar
-    # cuantos estudiantes pertenecen el grado "grade"
-    # Conectarse a la base de datos
-
-    conn = TinyMongoClient()
-    db = conn[db_name]
-
-    count = db.estudiante.find({"grade": grado}).count()
-
-    conn.close()
-    return count
 
 if __name__ == "__main__":
-    # Borrar DB
+    
     clear()
 
     fill()
 
-    # Buscar autor
     userId = 5
-    title_completed_count(userId)
+    total = title_completed_count(userId)
+    print("Cantidad de completados de este usuario:",userId, "Total:",total)
 
